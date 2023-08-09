bc52495e08519b085575fdb214521a4a6b2285c0
Harmedino <adamsahmed191@gmail.com>
2023-08-09 07:36:07 +0100

    home started

---
+++ src/component/HeaderBlog.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HeaderBlog.css"; // Import your CSS file
+
+const HeaderBlog = () => {
+  return (
+    <div className="home-container">
+      <header className="header">
+        <h1>My Awesome Blog</h1>
+        <p>Welcome to the place where words come to life!</p>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderBlog;
