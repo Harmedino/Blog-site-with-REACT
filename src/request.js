0f0b181ee09c671d84a581a05045841c0674892f
Harmedino <adamsahmed191@gmail.com>
2023-09-04 15:03:35 +0100

    image fixed

--- src/request.js
+++ src/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // local machine base url
 
-const BaseUrl = "http://localhost:5000/"
+export const BaseUrl = "http://localhost:5000/"
 
 export const publicRequest = () => {
     return axios.create({
