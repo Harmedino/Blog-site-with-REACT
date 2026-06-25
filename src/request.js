63509ded8fd5869a136b2f89b662c4696743e97d
snovae <adamsahmed191@gmail.com>
2026-06-25 14:02:49 +0100

    fix

--- src/request.js
+++ src/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // local machine base url
 
-export const BaseUrl = "https://ill-gold-dog-coat.cyclic.app/"
+export const BaseUrl = "https://blog-backend-node-7kjl.onrender.com/"
 //  export const BaseUrl = "http://localhost:5000/"
 
 export const publicRequest = () => {
